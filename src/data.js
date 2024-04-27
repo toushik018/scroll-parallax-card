a76646b6f06513eaa83b1c327f5317d778853169
toushik018 <tayebhossain018@gmail.com>
2024-04-27 21:43:33 +0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projectsData = [
+    {
+        "title": "Discover",
+        "description": "Explore new horizons with our diverse range of products.",
+        "src": "road.jpg",
+        "link": "https://images.unsplash.com/photo-1713950689683-ab592ce25707?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "color": "#BBACAF"
+    },
+    {
+        "title": "Connect",
+        "description": "Stay in touch and build relationships with our community.",
+        "src": "water.jpg",
+        "link": "https://images.unsplash.com/photo-1713815713124-362af0201f3c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "color": "#977F6D"
+    },
+    {
+        "title": "Achieve",
+        "description": "Reach your goals and fulfill your potential with our guidance.",
+        "src": "mountain.jpg",
+        "link": "https://images.unsplash.com/photo-1713781654565-ae4aef23f75b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "color": "#C2491D"
+    },
+    {
+        "title": "Inspire",
+        "description": "Ignite your creativity and imagination with our inspiring content.",
+        "src": "snow.jpg",
+        "link": "https://images.unsplash.com/photo-1713877561507-881bf3b1c310?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "color": "#B62429"
+    },
+    {
+        "title": "Transform",
+        "description": "Unlock new possibilities and transform your life with our innovative solutions.",
+        "src": "flowers.jpg",
+        "link": "https://images.unsplash.com/photo-1713761928558-89209d6a4d26?q=80&w=2043&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "color": "#88A28D"
+    }
+]
